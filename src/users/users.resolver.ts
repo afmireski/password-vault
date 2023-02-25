330c11388b454a307938a1c448d333258f89e2e7
afmireski <afmireski@outlook.com>
2023-02-25 15:28:31 -0300

    #9 - Implement createUser logic in service

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,7 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { Args, Info, Mutation, Resolver } from '@nestjs/graphql';
+import { GraphQLResolveInfo } from 'graphql';
+import { User } from 'prisma/@generated/user/user.model';
+import { CreateUserInput } from './dtos/create-user.input';
 import { UsersService } from './users.service';
 
 @Resolver()
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
+
+  @Mutation(() => User)
+  @UsePipes(ValidationPipe)
+  async CreateUser(
+    @Args('input') input: CreateUserInput,
+    @Info() info: GraphQLResolveInfo,
+  ) {
+    const select = null;
+
+    return this.usersService.createUser(input, select);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaSelect } from '@paljs/plugins';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserInput } from './dtos/create-user.input';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(input: CreateUserInput, select: PrismaSelect) {
+    const { email, name, password } = input;
+
+    const salt = bcrypt.genSaltSync(10);
+
+    return Promise.resolve(
+      this.prisma.user.create({
+        data: {
+          email,
+          name,
+          password: bcrypt.hashSync(password, salt),
+        },
+      }),
+    ).catch(() => {
+      throw new InternalServerErrorException('Falha ao criar o usuário');
+    });
+  }
 }
