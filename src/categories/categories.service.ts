ad268d58aa8cea7335494ea2a003fc5ef67d03d4
afmireski <afmireski@outlook.com>
2023-07-11 21:41:51 -0300

    #11 - Implement method 'createMethod'

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaRequest, PrismaResponse } from 'src/types/custom-types';
+import { CreateCategoryInput } from './dtos/create-category.input';
+import { CategoryDTO } from './dtos/category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createCategory(
+    request: PrismaRequest<CreateCategoryInput>,
+  ): PrismaResponse<CategoryDTO> {
+    const {
+      input: { user_id, name },
+      select,
+    } = request;
+
+    return Promise.resolve(
+      this.prisma.category.create({
+        data: {
+          user_id,
+          name,
+        },
+        ...select,
+      }),
+    ).catch(() => {
+        throw new InternalServerErrorException("Houve uma falha ao criar a categoria");
+    });
+  }
+}
---
+++ src/categories/dtos/create-category.input.ts
@@ -0,0 +1,14 @@
+import { Field } from '@nestjs/graphql';
+import * as Validator from 'class-validator';
+
+export class CreateCategoryInput {
+  @Field(() => String, { nullable: false })
+  @Validator.IsUUID('4')
+  @Validator.IsNotEmpty()
+  user_id!: string;
+
+  @Field(() => String, { nullable: false })
+  @Validator.IsUUID('4')
+  @Validator.Length(1, 50)
+  name!: string;
+}
