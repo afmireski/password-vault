8fcb8f4f6788416890d585b69ca264a8a80310e5
afmireski <afmireski@outlook.com>
2023-07-18 10:47:05 -0300

    #13 - Create 'PassrodDTO'

--- src/passwords/passwords.service.ts
+++ src/passwords/passwords.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaRequest } from 'src/types/custom-types';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreatePasswordInput } from './dtos/create-password.input';
 
 @Injectable()
-export class PasswordsService {}
+export class PasswordsService {
+    constructor(prisma: PrismaService) {}
+
+    async createPassword(request: PrismaRequest<CreatePasswordInput>): PrismaRequest<>
+}
