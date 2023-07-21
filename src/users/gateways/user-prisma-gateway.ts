ea07cefcfc681eccace6d4a8726d9b204b99d350
afmireski <afmireski@outlook.com>
2023-07-21 14:59:20 -0300

    #dev - Implement update in UserPrismaGateway

--- src/users/gateways/user-prisma-gateway.ts
+++ src/users/gateways/user-prisma-gateway.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { Request, Response } from '../../types/custom-types';
 import { FindUserByIdInput } from '../dtos/find-user-by-id.interface';
+import { UpdateUserInput } from '../dtos/update-user.input';
 import { UserDTO } from '../dtos/user.dto';
 import { UserPersistanceGateway } from './user-gateway.interface';
 
@@ -25,5 +26,62 @@ export class UserPrismaGateway implements UserPersistanceGateway {
     ).catch(() => {
       throw new BadRequestException('Usuário não encontrado');
     });
+  }
+
+  async update(request: Request<UpdateUserInput>): Response<UserDTO> {
+    const { input, select } = request;
+    const { user_id, name, email } = input;
+
+    const transactionArray = [];
+
+    const findUserQuery = this.prisma.user.findFirst({
+      where: {
+        id: {
+          equals: user_id,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    transactionArray.push(findUserQuery);
+
+    const verifyEmailQuery = this.prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    transactionArray.push(verifyEmailQuery);
+
+    return Promise.resolve(this.prisma.$transaction(transactionArray)).then(
+      async ([user, validation]) => {
+        if (!user) {
+          throw new BadRequestException('Usuário não encontrado');
+        } else if (validation) {
+          throw new BadRequestException(
+            `Já existe um usuário cadastrado com o email: ${email}.`,
+          );
+        }
+
+        return await this.prisma.user.update({
+          where: {
+            id: user_id,
+          },
+          data: {
+            name,
+            email: email,
+            updated_at: new Date(),
+          },
+          ...select,
+        });
+      },
+    );
   }
 }
