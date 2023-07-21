74091aa7a7767520240f43a6c9ced6913a286ef0
afmireski <afmireski@outlook.com>
2023-07-21 15:08:12 -0300

    #dev - Refactor delete in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,7 @@
-import {
-  BadRequestException,
-  Inject,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { Inject, Injectable } from '@nestjs/common';
 import { Request, Response } from '../types/custom-types';
 import { DeleteUserInput } from './dtos/delete-user.input';
 import { FindUserByIdInput } from './dtos/find-user-by-id.interface';
-import { FindUserGraphQLInput } from './dtos/find-user-graphql.input';
 import { UpdateUserInput } from './dtos/update-user.input';
 import { UserDTO } from './dtos/user.dto';
 import { UserPersistanceGateway } from './gateways/user-gateway.interface';
@@ -28,21 +21,7 @@ export class UsersService {
     return await this.userGateway.update(request);
   }
 
-  /* async deleteUserInput(request: Request<DeleteUserInput>): Response<UserDTO> {
-    const { input, select } = request;
-    const { user_id } = input;
-
-    return Promise.resolve(
-      this.prisma.user
-        .delete({
-          where: {
-            id: user_id,
-          },
-          ...select,
-        })
-        .catch(() => {
-          throw new InternalServerErrorException('Falha ao excluir o usuário!');
-        }),
-    );
-  } */
+  async deleteUserInput(request: Request<DeleteUserInput>): Promise<void> {
+    await this.userGateway.delete(request);
+  }
 }
