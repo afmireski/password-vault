4569b700ca3b68490b2bd3f27691bcd3ca18e4ab
afmireski <afmireski@outlook.com>
2023-07-22 15:31:06 -0300

    #dev - Configure User Module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { UserPrismaGateway } from './gateways/user-prisma-gateway';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersResolver, UsersService],
+  providers: [
+    UsersResolver,
+    UsersService,
+    UserPrismaGateway,
+    {
+      provide: 'UserPersistenceGateway',
+      useExisting: UserPrismaGateway,
+    },
+  ],
 })
 export class UsersModule {}
