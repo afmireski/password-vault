1b12f53338a9b3ac39148021d964aa169346d8d0
afmireski <afmireski@outlook.com>
2023-07-24 16:24:45 -0300

    #dev - Configure CategoryPersistanceGateway in categories module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesResolver } from './categories.resolver';
+import { CategoryPrismaGateway } from './gateways/category-prisma-gateway';
 
 @Module({
-  providers: [CategoriesResolver, CategoriesService],
+  providers: [
+    CategoriesResolver,
+    CategoriesService,
+    CategoryPrismaGateway,
+    {
+      provide: 'CategoryPersistanceGateway',
+      useExisting: CategoryPrismaGateway,
+    },
+  ],
 })
 export class CategoriesModule {}
