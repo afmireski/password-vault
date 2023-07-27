673fa051fd412a897da7bd863d4e2a1e6a41916d
afmireski <afmireski@outlook.com>
2023-07-26 22:39:35 -0300

    #dev - Implement ChangeAccountPassword end-point

--- src/change-account-password/change-account-password.resolver.ts
+++ src/change-account-password/change-account-password.resolver.ts
@@ -1,9 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Success } from 'src/dtos/success.dto';
+import { Response } from 'src/types/custom-types';
 import { ChangeAccountPasswordService } from './change-account-password.service';
+import { ChangeAccountPasswordGraphQLInput } from './dtos/change-account-password-graphql.input';
 
 @Resolver()
 export class ChangeAccountPasswordResolver {
   constructor(
     private readonly changeAccountPasswordService: ChangeAccountPasswordService,
   ) {}
+
+  @Mutation(() => Success)
+  @UsePipes(ValidationPipe)
+  async ChangeAccountPassword(
+    @Args('input') input: ChangeAccountPasswordGraphQLInput,
+  ): Response<Success> {
+    await this.changeAccountPasswordService.changeAccountPassword({ input });
+
+    return { success: true };
+  }
 }
